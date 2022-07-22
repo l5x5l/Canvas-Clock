8a8dbfb9e07f7e356985a1e9c6418e2093e21f9a
yunsehwan <sh10302@naver.com>
2022-07-22 15:40:23 +0900

    공통 요소 추가 (baseActivity, baseFragment, GlobalApplication)

---
+++ app/src/main/java/com/example/canvasclock/config/GlobalApplication.kt
@@ -0,0 +1,15 @@
+package com.example.canvasclock.config
+
+import android.app.Application
+import com.example.data.datasource.room.database.ClockDatabase
+import com.example.data.datasource.shared_preference.SharedPreference
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        ClockDatabase.initDB(this)
+        SharedPreference.setSharedPreference(getSharedPreferences("CANVAS_CLOCK", MODE_PRIVATE))
+    }
+}
