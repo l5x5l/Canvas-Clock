3f3c70884c64b43ccc2bf6150cd694c4a5d284d6
yunsehwan <sh10302@naver.com>
2022-09-27 09:58:39 +0900

    [수정] 위젯 생성시 위젯의 초기 크기와 관련없이 항상 90dp를 기준으로 그려졌던 문제 수정

--- app/src/main/java/com/example/canvasclock/ui/page/widget/WidgetActivity.kt
+++ app/src/main/java/com/example/canvasclock/ui/page/widget/WidgetActivity.kt
@@ -18,7 +18,7 @@ import com.example.canvasclock.databinding.ActivityClockListBinding
 import com.example.canvasclock.ui.page.clock_list.ClockListViewModel
 import com.example.canvasclock.ui.recycler.adapter.ClockListPagingAdapter
 import com.example.canvasclock.ui.recycler.decoration.Grid2Decoration
-import com.example.canvasclock.util.dpToPx
+import com.example.canvasclock.util.WidgetSizeProvider
 import com.example.canvasclock.util.drawClock
 import com.example.canvasclock.util.drawTimeHand
 import com.example.domain.models.ClockData
@@ -33,8 +33,6 @@ class WidgetActivity : BaseActivity<ActivityClockListBinding>(R.layout.activity_
     var widgetId = -1
     private var remoteView : RemoteViews ?= null
 
-    private var initWidgetSize = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -45,9 +43,6 @@ class WidgetActivity : BaseActivity<ActivityClockListBinding>(R.layout.activity_
         widgetId = intent?.extras?.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID) ?: AppWidgetManager.INVALID_APPWIDGET_ID
 
         binding.tvTitle.text = getString(R.string.choose_clock)
-
-        initWidgetSize = dpToPx(this, 90)
-
     }
 
     private fun setObserver() {
@@ -92,13 +87,13 @@ class WidgetActivity : BaseActivity<ActivityClockListBinding>(R.layout.activity_
     private fun itemClickEvent(clockData : ClockData){
         remoteView = RemoteViews(this.packageName, R.layout.widget_clock)
         val appWidgetManger = AppWidgetManager.getInstance(this@WidgetActivity)
+        val widgetSize = WidgetSizeProvider(this, appWidgetManger).getWidgetsSize(widgetId)
 
-        val widgetSize = initWidgetSize
-        val radius = initWidgetSize / 2
+        val radius = kotlin.math.min(widgetSize.first, widgetSize.second) / 2
 
-        val shapeBitmap = Bitmap.createBitmap(widgetSize, widgetSize, Bitmap.Config.ARGB_8888)
+        val shapeBitmap = Bitmap.createBitmap(widgetSize.first, widgetSize.second, Bitmap.Config.ARGB_8888)
         val shapeCanvas = Canvas(shapeBitmap)
-        drawClock(shapeCanvas, clockData.clockPartList, widgetSize/2, widgetSize/2, radius)
+        drawClock(shapeCanvas, clockData.clockPartList, widgetSize.first / 2, widgetSize.second / 2, radius)
 
         remoteView?.let {
             it.setImageViewBitmap(R.id.iv_widget_clock_shape, shapeBitmap)
@@ -106,9 +101,9 @@ class WidgetActivity : BaseActivity<ActivityClockListBinding>(R.layout.activity_
         }
 
         val calendar = Calendar.getInstance()
-        val handleBitmap = Bitmap.createBitmap(widgetSize, widgetSize, Bitmap.Config.ARGB_8888)
+        val handleBitmap = Bitmap.createBitmap(widgetSize.first, widgetSize.second, Bitmap.Config.ARGB_8888)
         val handleCanvas = Canvas(handleBitmap)
-        drawTimeHand(canvas = handleCanvas, clock = clockData, mx = widgetSize / 2, my = widgetSize / 2, radius = radius, is24HourMode = false, hour = calendar.get(Calendar.HOUR), minute = calendar.get(Calendar.MINUTE), second = null )
+        drawTimeHand(canvas = handleCanvas, clock = clockData, mx = widgetSize.first / 2, my = widgetSize.second / 2, radius = radius, is24HourMode = false, hour = calendar.get(Calendar.HOUR), minute = calendar.get(Calendar.MINUTE), second = null )
 
         remoteView?.let {
             it.setImageViewBitmap(R.id.iv_widget_clock_handle, handleBitmap)
