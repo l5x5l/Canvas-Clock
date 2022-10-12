fef64ccbd13e0699a9bfd7273defc5b7b97e7e04
yunsehwan <sh10302@naver.com>
2022-10-12 11:46:49 +0900

    [수정] 위젯 업데이트 함수를 모양/시침 업데이트로 분리

--- app/src/main/java/com/strayAlpaca/canvasclock/ui/widget/ClockWidgetProvider.kt
+++ app/src/main/java/com/strayAlpaca/canvasclock/ui/widget/ClockWidgetProvider.kt
@@ -9,7 +9,6 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.os.SystemClock
 import android.widget.RemoteViews
 import com.strayAlpaca.canvasclock.R
 import com.strayAlpaca.canvasclock.util.WidgetSizeProvider
@@ -26,6 +25,9 @@ import kotlin.math.min
 @AndroidEntryPoint
 class ClockWidgetProvider : AppWidgetProvider() {
 
+    private val clockWidgetUpdateShape = "com.strayAlpaca.canvasclock.UPDATE_SHAPE"
+    private val clockWidgetUpdateTimeHand = "com.strayAlpaca.canvasclock.UPDATE_TIME"
+
     private val firstAlarmId = 2
     private val job = SupervisorJob()
     private val coroutineScope = CoroutineScope(Dispatchers.IO + job)
@@ -37,57 +39,82 @@ class ClockWidgetProvider : AppWidgetProvider() {
     override fun onReceive(context: Context?, intent: Intent?) {
         super.onReceive(context, intent)
 
+        val appwidgetManager = AppWidgetManager.getInstance(context)
         if (intent?.action == AppWidgetManager.ACTION_APPWIDGET_UPDATE && context != null) {
-//            cancelAlarm(context)
-//            callAlarm(context, intent)
-            val appwidgetManager = AppWidgetManager.getInstance(context)
+            callAlarm(context, intent)
             val widgetIds = appwidgetManager.getAppWidgetIds(ComponentName(context.packageName, ClockWidgetProvider::class.java.name))
             onUpdate(context, appwidgetManager, widgetIds)
+        }
+
+        if (intent?.action == clockWidgetUpdateTimeHand && context != null) {
+            callAlarm(context, intent)
+            val widgetIds = appwidgetManager.getAppWidgetIds(ComponentName(context.packageName, ClockWidgetProvider::class.java.name))
+            drawClockTimeHand(context, appwidgetManager, widgetIds)
+        }
+
+        if (intent?.action == clockWidgetUpdateShape && context != null) {
+            val widgetIdx = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS)
+            if (widgetIdx?.isNotEmpty() == true) {
+                drawClockShape(context, appwidgetManager, widgetIdx)
+            }
         }
     }
 
     override fun onEnabled(context: Context?) {
         super.onEnabled(context)
         context?.let {
-            callRepeatAlarm(it)
+            val intent = Intent(context, ClockWidgetProvider::class.java)
+            intent.action = clockWidgetUpdateTimeHand
+            callAlarm(context, intent)
         }
     }
 
-    // 다시 시작시 배경이 안보임 + widget Activity 에서 시계 선택시 시침이 바로 표시안됨
-    override fun onUpdate(
-        context: Context?,
-        appWidgetManager: AppWidgetManager?,
-        appWidgetIds: IntArray?
-    ) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds)
+    private fun drawClockShape(context: Context,
+                       appWidgetManager: AppWidgetManager,
+                       appWidgetIds: IntArray) {
+        coroutineScope.launch {
+            for (id in appWidgetIds) {
+                val clockIdx = useCaseWidgetClock.getWidgetClock(widgetId = id)
+                // clockIdx == -1 인 경우에는 해당 시계가 현재 존재하지 않음을 의미
+                if (clockIdx != -1) {
+                    val clock = useCaseGetClock.execute(clockIdx)
+                    val widgetSize = WidgetSizeProvider(context, appWidgetManager).getWidgetsSize(id)
+                    val radius = min(widgetSize.first, widgetSize.second) / 2
+                    val shapeBitmap = Bitmap.createBitmap(widgetSize.first, widgetSize.second, Bitmap.Config.ARGB_8888)
+                    val shapeCanvas = Canvas(shapeBitmap)
 
-        if (context != null && appWidgetIds != null && appWidgetManager != null) {
-            coroutineScope.launch {
-                for (id in appWidgetIds) {
-                    val clockIdx = useCaseWidgetClock.getWidgetClock(widgetId = id)
-                     if (clockIdx != -1) {
-                        val clock = useCaseGetClock.execute(clockIdx)
-                        val widgetSize = WidgetSizeProvider(context, appWidgetManager).getWidgetsSize(id)
-                        val radius = min(widgetSize.first, widgetSize.second) / 2
-                        val handleBitmap = Bitmap.createBitmap(widgetSize.first, widgetSize.second, Bitmap.Config.ARGB_8888)
-                        val handleCanvas = Canvas(handleBitmap)
+                    withContext(Dispatchers.Default) {
+                        val remoteView = RemoteViews(context.packageName, R.layout.widget_clock)
+                        drawClock(canvas = shapeCanvas, clockPartList = clock.clockPartList, mx = widgetSize.first / 2, my = widgetSize.second / 2, radius = radius)
+                        remoteView.setImageViewBitmap(R.id.iv_widget_clock_shape ,shapeBitmap)
+                        appWidgetManager.partiallyUpdateAppWidget(id, remoteView)
+                    }
+                }
+            }
+        }
+    }
 
-                        val shapeBitmap = Bitmap.createBitmap(widgetSize.first, widgetSize.second, Bitmap.Config.ARGB_8888)
-                        val shapeCanvas = Canvas(shapeBitmap)
+    private fun drawClockTimeHand(context: Context,
+                          appWidgetManager: AppWidgetManager,
+                          appWidgetIds: IntArray) {
+        coroutineScope.launch {
+            for (id in appWidgetIds) {
+                val clockIdx = useCaseWidgetClock.getWidgetClock(widgetId = id)
+                // clockIdx == -1 인 경우에는 해당 시계가 현재 존재하지 않음을 의미
+                if (clockIdx != -1) {
+                    val clock = useCaseGetClock.execute(clockIdx)
+                    val widgetSize = WidgetSizeProvider(context, appWidgetManager).getWidgetsSize(id)
+                    val radius = min(widgetSize.first, widgetSize.second) / 2
+                    val handleBitmap = Bitmap.createBitmap(widgetSize.first, widgetSize.second, Bitmap.Config.ARGB_8888)
+                    val handleCanvas = Canvas(handleBitmap)
 
-                        withContext(Dispatchers.Default) {
-                            val calendar = Calendar.getInstance()
-                            val remoteView = RemoteViews(context.packageName, R.layout.widget_clock)
-
-                            drawTimeHand(canvas = handleCanvas, clock = clock, mx = widgetSize.first / 2, my = widgetSize.second / 2, radius = radius,
-                                hour = calendar.get(Calendar.HOUR_OF_DAY), minute = calendar.get(Calendar.MINUTE), second = null, is24HourMode = false )
-                            remoteView.setImageViewBitmap(R.id.iv_widget_clock_handle, handleBitmap)
-                            appWidgetManager.partiallyUpdateAppWidget(id, remoteView)
-
-                            drawClock(canvas = shapeCanvas, clockPartList = clock.clockPartList, mx = widgetSize.first / 2, my = widgetSize.second / 2, radius = radius)
-                            remoteView.setImageViewBitmap(R.id.iv_widget_clock_shape ,shapeBitmap)
-                            appWidgetManager.partiallyUpdateAppWidget(id, remoteView)
-                        }
+                    withContext(Dispatchers.Default) {
+                        val calendar = Calendar.getInstance()
+                        val remoteView = RemoteViews(context.packageName, R.layout.widget_clock)
+                        drawTimeHand(canvas = handleCanvas, clock = clock, mx = widgetSize.first / 2, my = widgetSize.second / 2, radius = radius,
+                            hour = calendar.get(Calendar.HOUR_OF_DAY), minute = calendar.get(Calendar.MINUTE), second = null, is24HourMode = false )
+                        remoteView.setImageViewBitmap(R.id.iv_widget_clock_handle, handleBitmap)
+                        appWidgetManager.partiallyUpdateAppWidget(id, remoteView)
                     }
                 }
             }
@@ -98,8 +125,7 @@ class ClockWidgetProvider : AppWidgetProvider() {
         super.onDisabled(context)
 
         context?.let {
-            //cancelAlarm(context)
-            cancelRepeatAlarm(it)
+            cancelAlarm(context)
         }
     }
 
@@ -120,7 +146,7 @@ class ClockWidgetProvider : AppWidgetProvider() {
         val pendingIntent = PendingIntent.getBroadcast(context, firstAlarmId, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE )
 
         val currentMillis = System.currentTimeMillis()
-        val nextTime = currentMillis - (currentMillis % 60000) + 60000  //ceil(System.currentTimeMillis() / 60000f).toLong() * 60000
+        val nextTime = currentMillis - (currentMillis % 60000) + 60000
 
         alarmManager.set(AlarmManager.RTC, nextTime, pendingIntent)
     }
@@ -135,26 +161,4 @@ class ClockWidgetProvider : AppWidgetProvider() {
             alarmManager.cancel(it)
         }
     }
-
-    private fun callRepeatAlarm(context : Context) {
-        val intent = Intent(context, ClockWidgetProvider::class.java)
-        intent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val pendingIntent = PendingIntent.getBroadcast(context, firstAlarmId, intent, PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE )
-
-        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 60000 ,60000, pendingIntent)
-    }
-
-    private fun cancelRepeatAlarm(context : Context) {
-        val intent = Intent(context, ClockWidgetProvider::class.java)
-        intent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val pendingIntent = PendingIntent.getBroadcast(context, firstAlarmId, intent, PendingIntent.FLAG_NO_CREATE or PendingIntent.FLAG_IMMUTABLE )
-
-        pendingIntent?.let{
-            alarmManager.cancel(it)
-            it.cancel()
-        }
-    }
-
 }
