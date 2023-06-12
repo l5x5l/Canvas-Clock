aeafaf0e0f49fcdcf23d995c836da1dd316538ec
yunsehwan <sh10302@naver.com>
2023-06-12 20:13:40 +0900

    [수정] onDraw에서 Paint객체를 생성하서 사용하던 방식을 클래스의 paint변수로 분리하도록 수정, 코드 가독성 개선

--- app/src/main/java/com/strayAlpaca/canvasclock/ui/custom_components/ClockInteractionModifyView.kt
+++ app/src/main/java/com/strayAlpaca/canvasclock/ui/custom_components/ClockInteractionModifyView.kt
@@ -20,13 +20,15 @@ import com.strayAlpaca.domain.models.ClockPartData
 import com.strayAlpaca.domain.models.Coordinate
 import kotlin.math.*
 
-class ClockInteractionModifyView(context : Context, attrs : AttributeSet) : FrameLayout(context, attrs) {
-    private val binding = ViewClockInteractionModifyBinding.inflate(LayoutInflater.from(context), this, true)
+class ClockInteractionModifyView(context: Context, attrs: AttributeSet) :
+    FrameLayout(context, attrs) {
+    private val binding =
+        ViewClockInteractionModifyBinding.inflate(LayoutInflater.from(context), this, true)
     private val pointMap = mutableMapOf<ClockPartPointAttr, Coordinate>()
 
     private val toRadian = Math.PI / 180
 
-    private lateinit var clockPartList : ArrayList<ClockPartData>
+    private lateinit var clockPartList: ArrayList<ClockPartData>
     private var mx = 0
     private var my = 0
     private var radius = 0
@@ -45,6 +47,9 @@ class ClockInteractionModifyView(context : Context, attrs : AttributeSet) : Fram
     private var relativeYPositionInViewStartTimePoint = 0f
     private var relativeXPositionInViewEndTimePoint = 0f
     private var relativeYPositionInViewEndTimePoint = 0f
+
+    private var guideLinePaint = Paint()
+    private var guideCirclePaint = Paint()
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
@@ -75,12 +80,12 @@ class ClockInteractionModifyView(context : Context, attrs : AttributeSet) : Fram
     }
 
     // 시간을 조정하는 인터렉션 버튼을 비활성화 합니다.
-    fun setTimeIntervalChangeButtonEnable(enabled : Boolean){
+    fun setTimeIntervalChangeButtonEnable(enabled: Boolean) {
         binding.viewbtnStartTimePoint.isEnabled = enabled
         binding.viewbtnEndTimePoint.isEnabled = enabled
     }
 
-    fun linkClockInfo(newClockPartData : ArrayList<ClockPartData>) {
+    fun linkClockInfo(newClockPartData: ArrayList<ClockPartData>) {
         clockPartList = newClockPartData
     }
 
@@ -105,18 +110,31 @@ class ClockInteractionModifyView(context : Context, attrs : AttributeSet) : Fram
     }
 
 
-    fun initModifyAction(setStartRadius : (Int) -> Unit, setMiddleRadius : (Int) -> Unit, setEndRadius : (Int) -> Unit, setTimeAngle : (Float, ClockPartTimeComponent) -> Unit, saveTimeAngle : () -> Unit) {
-        binding.viewbtnStartPoint.setOnTouchListener{ view, motionEvent ->
+    fun initModifyAction(
+        setStartRadius: (Int) -> Unit,
+        setMiddleRadius: (Int) -> Unit,
+        setEndRadius: (Int) -> Unit,
+        setTimeAngle: (Float, ClockPartTimeComponent) -> Unit,
+        saveTimeAngle: () -> Unit
+    ) {
+        binding.viewbtnStartPoint.setOnTouchListener { view, motionEvent ->
             when (motionEvent.action) {
                 MotionEvent.ACTION_DOWN -> {
                     relativeXPositionInViewStartPoint = motionEvent.x
                     relativeYPositionInViewStartPoint = motionEvent.y
                 }
+
                 MotionEvent.ACTION_MOVE -> {
                     val center = pointMap[ClockPartPointAttr.CENTER]!!
-                    val weight = min(sqrt((motionEvent.x + view.x + (view.width / 2) - relativeXPositionInViewStartPoint - center.x).pow(2) + (motionEvent.y + view.y + (view.height / 2) - relativeYPositionInViewStartPoint - center.y).pow(2)) / radius, 1f)
+                    val weight = min(
+                        sqrt(
+                            (motionEvent.x + view.x + (view.width / 2) - relativeXPositionInViewStartPoint - center.x).pow(2) +
+                                    (motionEvent.y + view.y + (view.height / 2) - relativeYPositionInViewStartPoint - center.y).pow(2)
+                        ) / radius, 1f
+                    )
                     setStartRadius((weight * 100).toInt())
                 }
+
                 MotionEvent.ACTION_UP -> {
                     view.performClick()
                 }
@@ -124,17 +142,27 @@ class ClockInteractionModifyView(context : Context, attrs : AttributeSet) : Fram
             true
         }
 
-        binding.viewbtnMiddlePoint.setOnTouchListener{ view, motionEvent ->
+        binding.viewbtnMiddlePoint.setOnTouchListener { view, motionEvent ->
             when (motionEvent.action) {
                 MotionEvent.ACTION_DOWN -> {
                     relativeXPositionInViewMiddlePoint = motionEvent.x
                     relativeYPositionInViewMiddlePoint = motionEvent.y
                 }
+
                 MotionEvent.ACTION_MOVE -> {
                     val center = pointMap[ClockPartPointAttr.CENTER]!!
-                    val weight = min(sqrt((motionEvent.x + view.x + (view.width / 2) - relativeXPositionInViewMiddlePoint - center.x).pow(2) + (motionEvent.y + view.y + (view.height / 2) - relativeYPositionInViewMiddlePoint - center.y).pow(2)) / radius, 1f)
+                    val weight = min(
+                        sqrt(
+                            (motionEvent.x + view.x + (view.width / 2) - relativeXPositionInViewMiddlePoint - center.x).pow(
+                                2
+                            ) + (motionEvent.y + view.y + (view.height / 2) - relativeYPositionInViewMiddlePoint - center.y).pow(
+                                2
+                            )
+                        ) / radius, 1f
+                    )
                     setMiddleRadius((weight * 100).toInt())
                 }
+
                 MotionEvent.ACTION_UP -> {
                     view.performClick()
                 }
@@ -142,17 +170,27 @@ class ClockInteractionModifyView(context : Context, attrs : AttributeSet) : Fram
             true
         }
 
-        binding.viewbtnEndPoint.setOnTouchListener{ view, motionEvent ->
+        binding.viewbtnEndPoint.setOnTouchListener { view, motionEvent ->
             when (motionEvent.action) {
                 MotionEvent.ACTION_DOWN -> {
                     relativeXPositionInViewEndPoint = motionEvent.x
                     relativeYPositionInViewEndPoint = motionEvent.y
                 }
+
                 MotionEvent.ACTION_MOVE -> {
                     val center = pointMap[ClockPartPointAttr.CENTER]!!
-                    val weight = min(sqrt((motionEvent.x + view.x + (view.width / 2) - relativeXPositionInViewEndPoint - center.x).pow(2) + (motionEvent.y + view.y + (view.height / 2) - relativeYPositionInViewEndPoint - center.y).pow(2)) / radius, 1f)
+                    val weight = min(
+                        sqrt(
+                            (motionEvent.x + view.x + (view.width / 2) - relativeXPositionInViewEndPoint - center.x).pow(
+                                2
+                            ) + (motionEvent.y + view.y + (view.height / 2) - relativeYPositionInViewEndPoint - center.y).pow(
+                                2
+                            )
+                        ) / radius, 1f
+                    )
                     setEndRadius((weight * 100).toInt())
                 }
+
                 MotionEvent.ACTION_UP -> {
                     view.performClick()
                 }
@@ -166,13 +204,18 @@ class ClockInteractionModifyView(context : Context, attrs : AttributeSet) : Fram
                     relativeXPositionInViewStartTimePoint = motionEvent.x
                     relativeYPositionInViewStartTimePoint = motionEvent.y
                 }
+
                 MotionEvent.ACTION_MOVE -> {
                     val center = pointMap[ClockPartPointAttr.CENTER]!!
-                    val radian = atan2((view.y + motionEvent.y + (view.height / 2) - relativeXPositionInViewStartTimePoint - center.y), (view.x + motionEvent.x + (view.width / 2) - relativeYPositionInViewStartTimePoint - center.x))
+                    val radian = atan2(
+                        (view.y + motionEvent.y + (view.height / 2) - relativeXPositionInViewStartTimePoint - center.y),
+                        (view.x + motionEvent.x + (view.width / 2) - relativeYPositionInViewStartTimePoint - center.x)
+                    )
                     val degree = (radian * 180 / Math.PI + 90).mod(360.0)
 
                     setTimeAngle(degree.toFloat(), ClockPartTimeComponent.START)
                 }
+
                 MotionEvent.ACTION_UP -> {
                     saveTimeAngle()
                     view.performClick()
@@ -188,13 +231,18 @@ class ClockInteractionModifyView(context : Context, attrs : AttributeSet) : Fram
                     relativeXPositionInViewEndTimePoint = motionEvent.x
                     relativeYPositionInViewEndTimePoint = motionEvent.y
                 }
+
                 MotionEvent.ACTION_MOVE -> {
                     val center = pointMap[ClockPartPointAttr.CENTER]!!
-                    val radian = atan2((view.y + motionEvent.y + (view.height / 2) - relativeXPositionInViewEndTimePoint - center.y), (view.x + motionEvent.x + (view.width / 2) - relativeYPositionInViewEndTimePoint - center.x))
+                    val radian = atan2(
+                        (view.y + motionEvent.y + (view.height / 2) - relativeXPositionInViewEndTimePoint - center.y),
+                        (view.x + motionEvent.x + (view.width / 2) - relativeYPositionInViewEndTimePoint - center.x)
+                    )
                     val degree = (radian * 180 / Math.PI + 90).mod(360.0)
 
                     setTimeAngle(degree.toFloat(), ClockPartTimeComponent.END)
                 }
+
                 MotionEvent.ACTION_UP -> {
                     saveTimeAngle()
                     view.performClick()
@@ -205,19 +253,24 @@ class ClockInteractionModifyView(context : Context, attrs : AttributeSet) : Fram
         }
     }
 
-    fun invalidateClock(){
+    fun invalidateClock() {
         invalidate()
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        var tempClockPart = ClockPartData()
+        var tempClockPart: ClockPartData? = null
 
-        if (canvas != null && ::clockPartList.isInitialized){
+        if (canvas != null && ::clockPartList.isInitialized) {
             pointMapSet = false
             for (clockPart in clockPartList) {
-                val coordinateClockPart = DomainLayerMapper.toCoordinateClockPartData(clockPartData = clockPart, viewMx = mx, viewMy = my, viewRadius = radius)
+                val coordinateClockPart = DomainLayerMapper.toCoordinateClockPartData(
+                    clockPartData = clockPart,
+                    viewMx = mx,
+                    viewMy = my,
+                    viewRadius = radius
+                )
 
                 drawClockPart(canvas, coordinateClockPart)
                 if (clockPart.uiState.isSelected && !pointMapSet) {
@@ -239,30 +292,64 @@ class ClockInteractionModifyView(context : Context, attrs : AttributeSet) : Fram
                 }
             }
 
+            if (tempClockPart == null) return
+
             // draw 순서 때문에 이곳에 배치, 가이드라인 작성부분
             if (isShowGuideline) {
                 if (!isMultipleModify) {
-                    val linePaint = Paint()
+                    val linePaint = guideLinePaint
                     linePaint.style = Paint.Style.STROKE
-                    linePaint.color = ContextCompat.getColor(context, R.color.guideline_color)//Color.parseColor("#3c000000")
+                    linePaint.color = ContextCompat.getColor(
+                        context,
+                        R.color.guideline_color
+                    )//Color.parseColor("#3c000000")
                     linePaint.strokeCap = Paint.Cap.ROUND
                     linePaint.strokeJoin = Paint.Join.ROUND
                     linePaint.strokeWidth = 6f
-                    canvas.drawLine(mx.toFloat(), my.toFloat(), pointMap[ClockPartPointAttr.START_TIME]!!.x, pointMap[ClockPartPointAttr.START_TIME]!!.y, linePaint)
-                    canvas.drawLine(mx.toFloat(), my.toFloat(), pointMap[ClockPartPointAttr.END_TIME]!!.x, pointMap[ClockPartPointAttr.END_TIME]!!.y, linePaint)
+                    canvas.drawLine(
+                        mx.toFloat(),
+                        my.toFloat(),
+                        pointMap[ClockPartPointAttr.START_TIME]!!.x,
+                        pointMap[ClockPartPointAttr.START_TIME]!!.y,
+                        linePaint
+                    )
+                    canvas.drawLine(
+                        mx.toFloat(),
+                        my.toFloat(),
+                        pointMap[ClockPartPointAttr.END_TIME]!!.x,
+                        pointMap[ClockPartPointAttr.END_TIME]!!.y,
+                        linePaint
+                    )
                 }
 
-                val circlePaint = Paint()
+                val circlePaint = guideCirclePaint
                 circlePaint.style = Paint.Style.STROKE
-                circlePaint.color = ContextCompat.getColor(context, R.color.guideline_color)//Color.parseColor("#3c000000")
+                circlePaint.color = ContextCompat.getColor(
+                    context,
+                    R.color.guideline_color
+                )//Color.parseColor("#3c000000")
                 circlePaint.strokeWidth = 6f
-                canvas.drawCircle(mx.toFloat(), my.toFloat(), tempClockPart.startRadius / 100f * radius, circlePaint)
+                canvas.drawCircle(
+                    mx.toFloat(),
+                    my.toFloat(),
+                    tempClockPart.startRadius / 100f * radius,
+                    circlePaint
+                )
                 if (tempClockPart.useMiddleRadius) {
-                    canvas.drawCircle(mx.toFloat(), my.toFloat(), tempClockPart.middleRadius / 100f * radius, circlePaint)
+                    canvas.drawCircle(
+                        mx.toFloat(),
+                        my.toFloat(),
+                        tempClockPart.middleRadius / 100f * radius,
+                        circlePaint
+                    )
                 }
-                canvas.drawCircle(mx.toFloat(), my.toFloat(), tempClockPart.endRadius / 100f * radius, circlePaint)
+                canvas.drawCircle(
+                    mx.toFloat(),
+                    my.toFloat(),
+                    tempClockPart.endRadius / 100f * radius,
+                    circlePaint
+                )
             }
-
 
 
         }
@@ -271,21 +358,31 @@ class ClockInteractionModifyView(context : Context, attrs : AttributeSet) : Fram
     }
 
     private fun changePointViewPosition() {
-        binding.viewbtnStartPoint.x = pointMap[ClockPartPointAttr.START]!!.x - (binding.viewbtnStartPoint.width / 2)
-        binding.viewbtnStartPoint.y = pointMap[ClockPartPointAttr.START]!!.y - (binding.viewbtnStartPoint.height / 2)
+        binding.viewbtnStartPoint.x =
+            pointMap[ClockPartPointAttr.START]!!.x - (binding.viewbtnStartPoint.width / 2)
+        binding.viewbtnStartPoint.y =
+            pointMap[ClockPartPointAttr.START]!!.y - (binding.viewbtnStartPoint.height / 2)
 
-        binding.viewbtnMiddlePoint.x = pointMap[ClockPartPointAttr.MIDDLE]!!.x - (binding.viewbtnMiddlePoint.width / 2)
-        binding.viewbtnMiddlePoint.y = pointMap[ClockPartPointAttr.MIDDLE]!!.y - (binding.viewbtnMiddlePoint.height / 2)
+        binding.viewbtnMiddlePoint.x =
+            pointMap[ClockPartPointAttr.MIDDLE]!!.x - (binding.viewbtnMiddlePoint.width / 2)
+        binding.viewbtnMiddlePoint.y =
+            pointMap[ClockPartPointAttr.MIDDLE]!!.y - (binding.viewbtnMiddlePoint.height / 2)
 
-        binding.viewbtnEndPoint.x = pointMap[ClockPartPointAttr.END]!!.x - (binding.viewbtnEndPoint.width / 2)
-        binding.viewbtnEndPoint.y = pointMap[ClockPartPointAttr.END]!!.y - (binding.viewbtnEndPoint.height / 2)
+        binding.viewbtnEndPoint.x =
+            pointMap[ClockPartPointAttr.END]!!.x - (binding.viewbtnEndPoint.width / 2)
+        binding.viewbtnEndPoint.y =
+            pointMap[ClockPartPointAttr.END]!!.y - (binding.viewbtnEndPoint.height / 2)
 
         if (!isMultipleModify) {
-            binding.viewbtnStartTimePoint.x = pointMap[ClockPartPointAttr.START_TIME]!!.x - (binding.viewbtnStartTimePoint.width / 2)
-            binding.viewbtnStartTimePoint.y = pointMap[ClockPartPointAttr.START_TIME]!!.y - (binding.viewbtnStartTimePoint.height / 2)
+            binding.viewbtnStartTimePoint.x =
+                pointMap[ClockPartPointAttr.START_TIME]!!.x - (binding.viewbtnStartTimePoint.width / 2)
+            binding.viewbtnStartTimePoint.y =
+                pointMap[ClockPartPointAttr.START_TIME]!!.y - (binding.viewbtnStartTimePoint.height / 2)
 
-            binding.viewbtnEndTimePoint.x = pointMap[ClockPartPointAttr.END_TIME]!!.x - (binding.viewbtnEndTimePoint.width / 2)
-            binding.viewbtnEndTimePoint.y = pointMap[ClockPartPointAttr.END_TIME]!!.y - (binding.viewbtnEndTimePoint.height / 2)
+            binding.viewbtnEndTimePoint.x =
+                pointMap[ClockPartPointAttr.END_TIME]!!.x - (binding.viewbtnEndTimePoint.width / 2)
+            binding.viewbtnEndTimePoint.y =
+                pointMap[ClockPartPointAttr.END_TIME]!!.y - (binding.viewbtnEndTimePoint.height / 2)
         }
     }
 }
